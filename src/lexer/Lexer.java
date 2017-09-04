f4aedc504804805fcc38db2986ffe8da35496730
Neumann Hon <neumannhon@Meepbook.local>
2017-09-03 20:48:00 -0400

    Changed comment indicator, fixed lexer recognising integers as identifiers, added javadocs

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -15,15 +15,30 @@ public class Lexer
     public static final String[] PRIMITIVES = {"Member_of_Z", "Character"};
     public static final String[] BREAK_STATEMENTS = {"DELIMIT", "Cease_pondering_of_possibilities"};
     public static final String[] THINGS = {"Function", "Class"};
-    public static final String[] COMMENT = {"#"};
+    public static final String[] COMMENT = {"Contemplation:"};
 
 
+    /**
+     * Takes in one line of PretentiousScript(tm) code and returns a list of tokens.
+     * Precondition: line cannot have trailing whitespace
+     *
+     * @param line: This is the line of PretentiousScript code that is to be converted into Tokens.
+     * @return Token: A list of Tokens representing the line of PretentiousScript code.
+     *
+     */
     public static List<Token> scanLine(String line)
     {
         //Handle Comments
-        if (line.charAt(0) == '#')
+        String[] components = line.split(" ");
+        if (components[0].equals("Contemplation:"))
         {
-            Token tk = new Token("COMMENT", line.substring(1, (line.length())));
+            //Token tk = new Token("COMMENT", line.substring(1, (line.length())));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < components.length; i++){
+                sb.append(components[i]);
+                sb.append(" ");
+            }
+            Token tk = new Token("COMMENT", sb.toString());
             List<Token> output = new ArrayList<>();
             output.add(tk);
             return output;
@@ -32,7 +47,6 @@ public class Lexer
         else
         {
             //Everything else
-            String[] components = line.split(" ");
             List<Token> output = new ArrayList<>();
             for (String component : components){
                 //Checking for primitive
@@ -41,7 +55,7 @@ public class Lexer
                     output.add(tk);
                 }
 
-                else if (component.matches("(\\w)*")){
+                else if (component.matches("(\\w)*") && !(component.matches("(\\d)*"))){
                     Token tk = new Token("IDENTIFIER", component);
                     output.add(tk);
                 }
@@ -50,9 +64,6 @@ public class Lexer
                     Token tk = new Token("OPERATOR", component);
                     output.add(tk);
                 }
-
-
-
             }
             return output;
 
@@ -61,12 +72,10 @@ public class Lexer
 
     public static void main(String[] args)
     {
-        List<Token> test = scanLine("Member_of_Z x = 69");
+        List<Token> test = scanLine("Contemplation: Asic is a meanie face");
         for (Token t : test){
             System.out.println(t);
         }
     }
-
-
 }
 
