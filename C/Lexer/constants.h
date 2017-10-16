29fab8061ab4ed89fd00b65c155159ba27dc67b5
Everybody0523 <h.neukc@gmail.com>
2017-10-16 09:35:59 -0400

    some regex strings

--- C/Lexer/constants.h
+++ C/Lexer/constants.h
@@ -1,3 +1,7 @@
 // Basically booleans
-const int MATCH = 1;
-const int NO_MATCH = 0;
+const int MATCH = 0;
+
+//Regular Expressions
+const char * validNames = "[[:alpha:]+[:print:]*]";
+const char * illegalNames = "[[:digit:]+[:print:]*]";
+const char * character = "\"[[:print:]]\"";
