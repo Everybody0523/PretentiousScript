2b93ae691edb09d32cdb4faf7fb0cf28f30846ec
Everybody0523 <h.neukc@gmail.com>
2017-10-16 10:10:58 -0400

    more constants

--- C/Lexer/constants.h
+++ C/Lexer/constants.h
@@ -1,6 +1,12 @@
 // Basically booleans
 const int MATCH = 0;
 
+//Primitives
+
+//COMMENT
+const char * comment1 = "Contemplation:"
+const char * comment1 = "contemplation:"
+
 //Regular Expressions
 const char * validNames = "[[:alpha:]+[:print:]*]";
 const char * illegalNames = "[[:digit:]+[:print:]*]";
