2e861583cc90569020b126dd9c9f28db891b56fc
Everybody0523 <h.neukc@gmail.com>
2017-10-16 10:08:24 -0400

    A new method

--- C/Lexer/Lexer.c
+++ C/Lexer/Lexer.c
@@ -6,7 +6,26 @@
 #include <regex.h>
 
 
+/*
+* Search the string array (ie. array of array of characters) for a string,
+* value. Return 0 if found, and 1 if not found.
+*/
+int searchArrayForString(const char * value, const char ** arr, size_t n)
+{
+    size_t i = 0;
+    int found=0;
+    for(i=0;i<length;i++){
+        if (strcmp(*(arr + i),value) == 0){
+            found = 1;
+        }
+    }
+    return found == 1 ? 0 : 1;
+}
 
+/*
+* Generate a (linked) List of TokenNodes from a line of PretentiousScript
+* code.
+*/
 TokenNode * scanLine(const char * line)
 {
     int l = strlen(line) + 1;
@@ -15,7 +34,8 @@ TokenNode * scanLine(const char * line)
     char * s;
     s = strtok(lineCopy, " ");
     int isComment = strcmp(s, "Contemplation:");
-    if (isComment == 0)
+    int mayAlsoBeComment = strcmp(s, "contemplation");
+    if (isComment == 0 || mayAlsoBeComment == 0)
     {
         // Handle comments
         Token * t1 = newToken("COMMENT", line);
@@ -24,6 +44,7 @@ TokenNode * scanLine(const char * line)
     }
     else // Everything else
     {
+
         return NULL;
     }
     return NULL;
@@ -31,8 +52,9 @@ TokenNode * scanLine(const char * line)
 }
 
 
-int main() {
-    const char * t = "Contemplation: MEMBER_OF_Z x = 340";
+int main()
+{
+    const char * t = "MEMBER_OF_Z x = 340";
     TokenNode * meep = scanLine(t);
     printf("%s\n", meep->value->value);
     return 0;
