7f93604e71388af990d04f08f06863f039af8df4
Everybody0523 <h.neukc@gmail.com>
2017-10-19 00:38:57 -0400

    more constats

--- C/Lexer/constants.h
+++ C/Lexer/constants.h
@@ -3,13 +3,13 @@ const int MATCH = 0;
 
 //Primitives
 const char PRIMITIVES [2][32];
-prims[0] = "MEMBER_OF_Z";
-prims[1] = "Character";
+PRIMITIVES[0] = "MEMBER_OF_Z";
+PRIMITIVES[1] = "Character";
 
 const char CONDITIONALS[3][32];
-prims[0] = "Suppose"; //If
-prims[1] = "Alternatively"; //Else If
-prims[2] = "Otherwise"; //Else
+CONDITIONALS[0] = "Suppose"; //If
+CONDITIONALS[1] = "Alternatively"; //Else If
+CONDITIONALS[2] = "Otherwise"; //Else
 
 //COMMENT
 const char * COMMENT0 = "Contemplation:"
@@ -17,5 +17,14 @@ const char * COMMENT1 = "contemplation:"
 
 //Regular Expressions
 const char * VALIDNAMES = "[[:alpha:]+[:print:]*]";
+//A valid name for a variable or function begins with an alphanumeric
+// character, then a series of printable characters. Something like 1a, or -a
+// is not a valid name for a variable or function.
+
+//TODO: Modify regular expression to support things like _meep
+
 const char * ILLEGALNAMES = "[[:digit:]+[:print:]*]";
+// Illegal names begin with a digit. Things that being with digits are bad.
+
 const char * CHARACTER = "\"[[:print:]]\"";
+// Characters consist of a single character enclosed in quotation marks.
